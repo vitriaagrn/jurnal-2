54b0d6828f1cdc3d09aab647434b5da1b0dfa580
vitriaagrn <vitriaagrn@student.telkomuniversity.ac.id>
2020-09-16 12:08:30 +0700

    latihan.java

---
+++ latihan.java
@@ -0,0 +1,7 @@
+class latihan{
+        public static void main(String[] args){
+		System.out.println("Nama:Vitria Anggraeni");
+		System.out.println("NIM:6706202048");
+		System.out.println("Kelas:D3IF44-02");
+		}
+}		
